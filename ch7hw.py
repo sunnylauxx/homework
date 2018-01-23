2d0fd4a01a1933c4f5ff73a07a3687454ed3e9d6
Sunny Lau <31958667+sunnylauxx@users.noreply.github.com>
2018-01-23 09:29:08 -0500

    hw

--- ch7hw.py
+++ ch7hw.py
@@ -1,4 +1,7 @@
 import sys
+import turtle
+wn = turtle.Screen()
+s = turtle.Turtle()
 
 def test(did_pass):
     """  Print the result of a test.  """
@@ -15,6 +18,7 @@ def test_suite():
     
 mylist = [12, 8, 13, 4, 83, -72, 17]
 mylist2 = ["fneo", "cneif","ncoe","ncis" "cmoejsw","wrrf"]
+
 def count_odd(numlist):
     count=0
     for n in numlist:
@@ -63,6 +67,62 @@ def sam(numlist):
             count += 1
     return count
 
+def sqrt(n):
+    """Ex 7:Newtons square root function -"""
+    approx = n/2.0     # Start with some or other guess at the answer
+    while True:
+        better = (approx + n/approx)/2.0
+        print("better",better)
+        if abs(approx - better) < 0.001:
+            return better
+        approx = better
+
+
+print("sqrt",sqrt(36.0))
+
+def is_prime(n):
+    """Write a function, is_prime, which takes a single integer
+    argument and returns True when the argument is a prime number and False otherwise"""
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
+
+        
+"""Revisit the drunk pirate problem from the exercises in chapter 3. This time,
+the drunk pirate makes a turn, and then takes some steps forward, and repeats this.
+Our social science student now records pairs of data: the angle of each turn, and the
+number of steps taken after the turn. Her experimental data is [(160, 20), (-43, 10),
+(270, 8), (-43, 12)]. Use a turtle to draw the path taken by our drunk friend.
+"""
+
+import turtle
+wn = turtle.Screen()
+fun = turtle.Turtle()
+
+steps = [(160, 20), (-43, 10), (270, 8), (-43, 12)]
+
+for (angle, moves) in steps:
+    fun.right(angle)
+    fun.forward(moves)
+ 
+    
+"""Many interesting shapes can be drawn by the turtle by giving a list of pairs like we did
+above, where the first item of the pair is the
+angle to turn, and the second item is the distance to move forward. Set up a list of pairs
+so that the turtle draws a house with a cross through the centre, as show here. This should
+be done without going over any of the lines / edges more than once, and without lifting your
+pen."""
+
+import turtle
+wn = turtle.Screen()
+fun = turtle.Turtle()
+fun.pensize(10)
+house = [(270,50), (30, 50), (120,50), (120,50), (225,70.1), (225,50), (225,70.1), (225,50) ]
+
+for (angle,moves) in house:
+    fun.right(angle)
+    fun.forward(moves)
 
 
 
